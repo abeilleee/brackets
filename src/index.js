f61606eb24f125215931dceda04653bdcb37d136
May <abeillee@bk.ru>
2024-10-09 15:35:25 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const OBJ = {};
+  const OPEN_BRACKETS = [];
+  let stack = [];
+
+  for (arr of bracketsConfig) {
+    OBJ[arr[1]] = arr[0];
+    OPEN_BRACKETS.push(arr[0])
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let top = stack[stack.length - 1]
+    if (OPEN_BRACKETS.includes(str[i])) {
+      if (str[i] === OBJ[str[i]] && str[i] === top) {
+        stack.pop();
+      } else {
+        stack.push(str[i])
+      }
+    } else {
+      if (stack.length === 0) {
+        return false;
+      } else {
+        if (top === OBJ[str[i]]) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    } 
+  }return stack.length === 0;
 }
